fe0016c4acc792387dd8fee551a037bfa7e1a3c6
Alex Bates <>
2024-07-13 10:38:48 +0100

    feat: working redirect to the guardian

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,40 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    if (details.url.includes("dailymail.co.uk")) {
-      return { redirectUrl: "https://www.theguardian.com/" };
-    }
+const redirectRule: chrome.declarativeNetRequest.Rule = {
+  id: 1,
+  priority: 1,
+  action: {
+    type: chrome.declarativeNetRequest.RuleActionType.REDIRECT,
+    redirect: {
+      url: "https://www.theguardian.com",
+    },
   },
-  {
-    urls: ["*://*.dailymail.co.uk/*"],
+  condition: {
+    urlFilter: "||dailymail.co.uk",
+    resourceTypes: [chrome.declarativeNetRequest.ResourceType.MAIN_FRAME],
   },
-  ["blocking"],
-);
+};
+
+async function updateRedirectRules() {
+  try {
+    await chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: [redirectRule.id],
+    });
+
+    await chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: [redirectRule],
+    });
+
+    console.log("Redirect rule updated successfully");
+  } catch (error) {
+    console.error("Error updating redirect rules", error);
+  }
+}
+
+chrome.runtime.onInstalled.addListener(() => updateRedirectRules());
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "updateRedirectUrl") {
+    redirectRule.action.redirect!.url = message.newUrl;
+    updateRedirectRules();
+    sendResponse({ success: true });
+  }
+});
